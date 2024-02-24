c354447a48cd3fbf4cc8d3ec10ab8dea2742daf1
yoo-hyunseung <hsung122@naver.com>
2024-02-24 18:51:32 +0900

    npm init 개발환경 셋팅

---
+++ react_start_npm/src/App.js
@@ -0,0 +1,12 @@
+// import react
+import React from 'react';
+
+const App = () =>{
+    return (
+        <div>
+            <h1>Hello React</h1>
+            <p>JSX React Component</p>
+        </div>
+    )
+}
+export default App;
---
+++ react_start_npm/src/Test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// rsc tap
+const Test = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Test;
